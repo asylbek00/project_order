5972bc5441d686855be41ffcf9dbb93d3825b0c8
Asylbek <asylbek310800@gmail.com>
2022-11-01 00:19:08 +0600

    added content info with components

---
+++ src/components/Meals/MealItem.js/MealItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from './MealItem.module.css'
+
+function MealItem(props) {   // или ({name, description , price})   и будет без пропс
+    const price = props.price.toFixed(2)
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>                 
+        <p className={classes.description}>{props.description}</p>
+        <p className={classes.price}>{price}$</p>
+      </div>
+    </li>
+  );
+}
+
+export default MealItem;
