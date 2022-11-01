3c38bd2d72ab18e3d939e8a8f717f29f6fbf8db9
Asylbek <asylbek310800@gmail.com>
2022-11-02 01:24:48 +0600

    added count

--- src/components/Meals/MealItem.js/MealItem.jsx
+++ src/components/Meals/MealItem.js/MealItem.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Counter from "./Counter";
-import classes from './MealItem.module.css'
+import classes from "./MealItem.module.css";
+// import MealItemForm from "./MealItemForm";
 
-function MealItem(props) {   // или ({name, description , price})   и будет без пропс
-    const price = props.price.toFixed(2)
+function MealItem(props) {
+  // или ({name, description , price})   и будет без пропс
+  const price1 = props.price.toFixed(2);
+  // console.log(price1);
+
   return (
     <li className={classes.meal}>
       <div>
-        <h3>{props.name}</h3>                 
+        <h3>{props.name}</h3>
         <p className={classes.description}>{props.description}</p>
-        <p className={classes.price}>{price}$</p>
+        <p className={classes.price}>{price1}$</p>
       </div>
-      <Counter/>
+      <Counter price2={price1}/>
+
+      {/* <div>
+        <MealItemForm />
+      </div> */}
     </li>
-    
   );
 }
 
